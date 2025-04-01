27d3cb6ef8be51dedf908d978bcc4c30ab1a1e66
Darcy <darcymcbeth@proton.me>
2025-04-01 12:47:03 +0100

    Written and ready to test.

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 fn CountFiles(images: &str) {
     let mut count = 0;
     for image in images.lines() {
@@ -8,7 +10,17 @@ fn CountFiles(images: &str) {
     println!("Total number of image files: {}", count);
 }
 
+fn openImages(images: &str) {
+    let arr: Box<[i32]> = Box::new([0; 10]);
+    for image in images.lines() {
+        if image.ends_with(".png") || image.ends_with(".jpg") {
+            let mut arr = File::open(image).expect("Failed to open file");
+        }
+    }
+}
+
 fn main() {
     let images = "./images/*";
     let countingFiles = CountFiles(images);
+    let openFiles = openImages(images);
 }
